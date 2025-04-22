53092a34d76182a11aaee66321494f2bb7b21212
valeedkhanjadoon <valeedkhanjadoon@gmail.com>
2025-04-22 20:19:21 +0200

    Lab01 - Solution Part 01

--- lock_server.h
+++ lock_server.h
@@ -8,16 +8,26 @@
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
+#include <map>
+#include <pthread.h>
 
 class lock_server {
 
  protected:
   int nacquire;
+  enum lock_states {FREE, LOCKED};
+  std::map<lock_protocol::lockid_t, lock_states> locks_vault;
+  pthread_mutex_t locks_vault_mutex;
+  pthread_cond_t locks_vault_state_cv;
 
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server();
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+
+  // Lab 01 Changes
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
--- rpc/rpc.cc
+++ rpc/rpc.cc
@@ -562,6 +562,17 @@ rpcs::add_reply(unsigned int clt_nonce, unsigned int xid,
 		char *b, int sz)
 {
 	ScopedLock rwl(&reply_window_m_);
+	// Changes of Lab One
+	std::list<reply_t>::iterator iter = reply_window_[clt_nonce].begin();
+     	for(;iter != reply_window_[clt_nonce].end(); ++iter){
+          reply_t reply = *iter;
+          if(reply.xid == xid){
+            iter->buf = b;
+            iter->sz = sz;
+            iter->cb_present = true;
+            break;
+          }
+        }
 }
 
 void
@@ -585,8 +596,40 @@ rpcs::checkduplicate_and_update(unsigned int clt_nonce, unsigned int xid,
 		unsigned int xid_rep, char **b, int *sz)
 {
 	ScopedLock rwl(&reply_window_m_);
+	std::list<reply_t>::iterator iter = reply_window_[clt_nonce].begin();
+	bool isForgotten = true;
+	bool isEmpty = reply_window_[clt_nonce].empty();
+	jsl_log(JSL_DBG_2, "rpcs::checkduplicate_and_update request: xid:%u, clt:%u\n", xid, clt_nonce);
+	while (iter != reply_window_[clt_nonce].end()) {
+	  reply_t reply = *iter;
+	  if (reply.xid < xid_rep) {
+	    iter = reply_window_[clt_nonce].erase(iter);
+	  }
+	  else {
+	    if (reply.xid == xid) {
+	      if (reply.cb_present) {
+	        *b = reply.buf;
+	        *sz = reply.sz;
+	        return DONE;
+	      }
+	      else return INPROGRESS;
+	    }
+	    else if (reply.xid < xid) {
+	      isForgotten = false;
+	    }
+	    iter++;
+	  }
+	}
 
-	return NEW;
+	if (isEmpty || !isForgotten){
+	  reply_t reply(xid);
+	  reply_window_[clt_nonce].push_back(reply);
+	  return NEW;	
+	}
+	else {
+	  jsl_log(JSL_DBG_2, "rpcs::checkduplicate_and_update:forgotten, xid:%u, clt:%u\n", xid, clt_nonce);
+	  return FORGOTTEN;	
+	}
 }
 
 //rpc handler
