72486b1db544cdada0a43d519faaa9371b1a4130
valeedkhanjadoon <70755199+valeedkhanjadoon@users.noreply.github.com>
2025-05-06 18:20:55 +0500

    [Lab 02] Practice code - Implemented CREATE operation

--- yfs_client.cc
+++ yfs_client.cc
@@ -25,6 +25,19 @@ yfs_client::n2i(std::string n)
   return finum;
 }
 
+yfs_client::inum
+yfs_client::generate_inum(bool isfile) {
+  inum file_inum;
+  file_inum = std::rand(); // generate a random integer
+  if (isfile) {
+    file_inum |= 0x80000000; // 10000000 00000000 00000000 00000000 = 0x80000000
+  } else {
+    file_inum &= 0x7FFFFFFF; // 01111111 11111111 11111111 11111111 = 0x7FFFFFFF
+  }
+
+  return file_inum;
+}
+
 std::string
 yfs_client::filename(inum inum)
 {
@@ -91,5 +104,73 @@ yfs_client::getdir(inum inum, dirinfo &din)
   return r;
 }
 
+/*
+  Create (Create a file, this is only for creating a file)
+  In create method, we follow the following steps:
+  1. Generate a unique inum for the file
+  2. Save the file
+  3. Once you have saved the file, you need to update the directory
+     and let it know that there is a new file.
+  4. Take the former content (list) previously kept in a temp variable
+     and append to it the new file addition record.
+*/
+int yfs_client::create(inum parent_inum, const char * name, inum & file_inum, bool isfile) {
+  status ret;
+
+  // Give a unique inum to the file/directory
+  file_inum = yfs_client::generate_inum(name,isfile);
+
+  // Initial content is for when the file is created
+  std::string initial_content;
+
+  // Former content is for saving the previous content of the directory
+  std::string former_content;
+
+  // yfs_client::dirent is the structure of how the records are stored in the directory
+  struct yfs_client::dirent file_dirent;
+
+  file_dirent.name = name;
+  file_dirent.inum = file_inum;
+
+  // [PUT] Save the file to the server
+  if (ec -> put(file_inum, initial_content) != extent_protocol::OK){
+    ret = IOERR;
+    goto release;
+  }
+
+  // [GET] Get former records of the directory
+  if (ec -> get(parent_inum, former_content) != extent_protocol::OK){
+    ret = IOERR;
+    goto release;
+  }
+
+  // Append the new file record to the former content
+  former_content.append(yfs_client::serialize_dirent(file_dirent));
+
+  // Update the directory structure
+  if (ec -> put(parent_inum, former_content) != extent_protocol::OK){
+    ret = IOERR;
+    goto release;
+  }
+
+  ret = yfs_client::OK;
+
+  release:
+    return ret;
+}
 
 
+
+/*
+  So, you take an object and convert it into a string.
+  Similar to a key value pair.
+*/
+std::string
+yfs_client::serialize_dirent(yfs_client::dirent dirent){
+  std::string result;
+  result.append(filename(dirent.inum));
+  result.append(":");
+  result.append(dirent.name);
+  result.append(";");
+  return result;
+}
